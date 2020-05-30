c0569b4b02955b04f77581808d8026d1242dda84
spring-raining <harusamex.com@gmail.com>
2020-05-31 07:26:59 +0900

    Place examples on public

---
+++ pages/examples/[title].tsx
@@ -0,0 +1,23 @@
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import * as UI from '../../src/components/ui';
+
+export default () => {
+  const router = useRouter();
+  const { title } = router.query;
+  const Asteroid = dynamic(() => import(`../../public/examples/${title}.mdx`), {
+    ssr: false,
+  });
+
+  return (
+    <div>
+      <Head>
+        <title>Asteroid</title>
+      </Head>
+      <UI.Box maxW={920} mx="auto" px={2}>
+        <Asteroid />
+      </UI.Box>
+    </div>
+  );
+};
