7761cc779d7ebc623ea23ecca970f7bf3d4c811d
spring-raining <harusamex.com@gmail.com>
2021-08-13 15:36:38 +0900

    chore: Fix build error

--- packages/transpiler/src/config.ts
+++ packages/transpiler/src/config.ts
@@ -1,7 +1,6 @@
 import { ServiceOptions } from 'esbuild-wasm';
-import * as packageJson from '../package.json';
 
 export const defaultConfig: ServiceOptions = {
-  wasmURL: `https://cdn.jsdelivr.net/npm/esbuild-wasm@${packageJson.dependencies['esbuild-wasm']}/esbuild.wasm`,
+  wasmURL: `https://cdn.jsdelivr.net/npm/esbuild-wasm@0.8.50/esbuild.wasm`,
   worker: true,
 };
